a80c646b884880c676e9a4b32d3e3316da26def4
chavangorakh1999 <chavangorakh19999@gmail.com>
2023-03-30 20:47:05 +0530

    basics

--- app.ts
+++ app.ts
@@ -1,8 +1,13 @@
-function add(num1:number,num2: number){
- return num1+num2;
+function add(num1:number,num2: number,boolflag:boolean){
+    if(boolflag){
+        console.log(num1+num2);
+    }else{
+        return num1+num2;
+    }
 }
 
 const number1=5;
 const number2=2.7;
-const result=add(number1,number2);
-console.log(result) 
+const boolflag=true;
+const result=add(number1,number2,boolflag);
+console.log(result)
